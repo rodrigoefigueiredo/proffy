a5ea6092523196f23c22b229f4844efb51aca126
Rodrigo Figueiredo <rodrigoefigueiredo@gmail.com>
2020-08-12 11:31:17 -0300

    Desenvolvimento do projeto web

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,41 +1,58 @@
-import React from 'react';
-import logoImg from '../../assets/images/logo.svg';
-import landingImg from '../../assets/images/landing.svg';
-import studyIcon from '../../assets/images/icons/study.svg';
-import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
-import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
-import './style.css'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+import "./style.css";
+
+import logoImg from "../../assets/images/logo.svg";
+import landingImg from "../../assets/images/landing.svg";
+import studyIcon from "../../assets/images/icons/study.svg";
+import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
+import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
+import api from "../../services/api";
 
 function Landing() {
-    return (
-        <div id="page-landing">
-            <div id="page-landing-content" className="container">
-                <div className="logo-container">
-                    <img src={logoImg} alt="Proffy"/>
-                    <h2>Sua plataforma de estudos online.</h2>
-                    <img 
-                        src={landingImg} 
-                        alt="Plataforma de estudos" 
-                        className="hero-image"/>
+  const [totalConnections, setTotalConnections] = useState(0);
+  
+  useEffect(() => {
+    api.get('connections').then(response => {
+      const { total } = response.data;
 
-                </div>
-                <div className="buttons-container">
-                    <a href="" className="study">
-                        <img src={studyIcon} alt="Estudar"/>
-                        Estudar
-                    </a>
-                    <a href="" className="give-classes">
-                        <img src={giveClassesIcon} alt="Dar Aulas"/>
-                        Dar Aulas
-                    </a>
-                </div>
-                <span className="total-connections">
-                    Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
-                </span>
-                
-            </div>
+      setTotalConnections(total);
+    })
+  }, []);
+
+  return (
+    <div id="page-landing">
+      <div className="container" id="page-landing-content">
+        <div className="logo-container">
+          <img src={logoImg} alt="Proffy" />
+          <h2>Sua Plataforma de estudos online</h2>
         </div>
-    )
+
+        <img
+          src={landingImg}
+          alt="Plataforma de Estudos"
+          className="hero-image"
+        />
+
+        <div className="buttons-container">
+          <Link to="/study" className="study">
+            <img src={studyIcon} alt="Estudar" />
+            Estudar
+          </Link>
+          <Link to="/give-classes" className="give-classes">
+            <img src={giveClassesIcon} alt="Dar Aulas" />
+            Dar Aulas
+          </Link>
+        </div>
+
+        <span className="total-connections">
+          Total de {totalConnections} conexões realizadas.{" "}
+          <img src={purpleHeartIcon} alt="Coração Roxo" />
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default Landing;
